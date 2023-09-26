d4416b6375f9b4172bcecb9c42c28b89cf856c71
TDavid <tomik.david1@gmail.com>
2023-09-26 08:40:24 +0200

    Bejegyzes Class létrehozása és f2/b rész elkészitése

---
+++ TomikDávid_BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomikDávid_BejegyzesProjekt
+{
+    internal class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int likeok;
+        private DateTime letrejot;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom, DateTime letrejot)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejot = letrejot;
+            this.szerkesztve = DateTime.Now;
+        }
+
+        public string Szerzo { get => szerzo;}
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Likeok { get => likeok;}
+        public DateTime Letrejot { get => letrejot;}
+        public DateTime Szerkesztve { get => szerkesztve; }
+
+        public void Like()
+        {
+            this.likeok++;
+        }
+        public override string ToString()
+        {
+            return $"{this.szerzo} - Likeok száma:{this.likeok} - Létrejöt:{this.letrejot} \n{szerkesztve} \n {tartalom}";
+        }
+    }
+}
--- TomikDávid_BejegyzesProjekt/Program.cs
+++ TomikDávid_BejegyzesProjekt/Program.cs
@@ -8,8 +8,29 @@ namespace TomikDávid_BejegyzesProjekt
 {
     internal class Program
     {
+        List<Bejegyzes> list = new List<Bejegyzes>();
+        List<Bejegyzes> list2 = new List<Bejegyzes>();
         static void Main(string[] args)
         {
+            Console.ReadKey();
+        }
+
+
+
+        public void f2_b()
+        {
+            Console.Write("Kérlek adjón meg egy számot menyi bejegyzést akkar létrehozni!");
+            int szam = int.Parse(Console.ReadLine());
+            for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine("Kérlek adja meg a szerzot:");
+                string szerzo = Console.ReadLine();
+                Console.WriteLine("Kérlek adja meg a tartalmat");
+                string tartalom = Console.ReadLine();
+                DateTime dateTime = DateTime.Now;
+                Bejegyzes b = new Bejegyzes(szerzo,tartalom,dateTime);
+                list.Add(b);
+            }
         }
     }
 }
